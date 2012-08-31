297a394dc1cbc104c81f05e9b5c8e654f2967f42
Kevin Qi <iqnivek@gmail.com>
2012-08-30 22:42:09 -0400

    Modify comment

--- lib/deep_freeze/object.rb
+++ lib/deep_freeze/object.rb
@@ -1,5 +1,5 @@
 class Object
-  # Freeze then deepen to prevent infinite recursion
+  # Freeze objects recursively, so nested objects get frozen too.
   def deep_freeze
     unless frozen?
       freeze unless is_a?(Module)
